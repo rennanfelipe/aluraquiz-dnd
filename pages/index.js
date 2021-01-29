0abf44356473049b222a02617f09d83d2f1fcbeb
rennanfelipe <rennan.chaves95@gmail.com>
2021-01-28 21:07:12 -0300

    Added player name

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import Head from 'next/Head';
 import { useRouter } from 'next/router'
 
@@ -7,21 +6,11 @@ import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-import { useState } from 'react';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  color: ${db.primary};
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
 	const router = useRouter();
@@ -48,14 +37,18 @@ export default function Home() {
 				console.log('A que ponto chegamos?');
 			}
 			}>
-				<input placeholder='Seu nome' onChange={ (event) => {
+				<Input
+					name='Nome do jogador'
+					value=''
+					placeholder='Seu nome' 
+					onChange={ (event) => {
 					setName(event.target.value);
 					console.log(name);
 				}
 				} />
-				<button type='submit' disabled={name.length === 0}>
-					Bora jogar { name }?
-				</button>
+				<Button type='submit' disabled={name.length === 0}>
+					{`Bora jogar ${ name }?`}
+				</Button>
 			</form>
           </Widget.Content>
         </Widget>
